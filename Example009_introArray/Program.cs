f0c2633b11b71b4b4b74bf366621970ffb7bcd01
Chernakov Ilya <chernakov.ilya@gmail.com>
2022-09-18 10:25:03 +0300

    add example 009

---
+++ Example009_introArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+int[] array = {51, 32, 31, 24, 45, 66, 37, 38, 99};
+array[0] = 12; //Присвоить первому элементу массива array значение
+
+int result = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
+
+Console.WriteLine(result);
